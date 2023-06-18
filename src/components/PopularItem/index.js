2a98f1f3a62743a00e5e84409ab90b208929996c
komalachintha <chinthakomala7@gmail.com>
2023-06-18 02:38:12 +0000

    message

--- src/components/PopularItem/index.js
+++ src/components/PopularItem/index.js
@@ -128,7 +128,7 @@ class PopularItem extends Component {
             }
           }
 
-          return <div testid="trending">{getResult()}</div>
+          return <div data-testid="trending">{getResult()}</div>
         }}
       </MovieContext.Consumer>
     )
